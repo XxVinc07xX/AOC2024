1bcada9b37d14a392528a38858287cb18ae2aaeb
vw <vincent.wuillaume@student.uclouvain.be>
2024-12-05 11:47:14 +0100

    d2 pt2 done

--- d2.py
+++ d2.py
@@ -3,59 +3,63 @@ lines = input.readlines()
 
 safe = 0
 
+def safe_rm_inc(line):
+    for i in range(1,len(line)):
+        diff = line[i] - line[i-1]
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+def safe_rm_dec(line):
+    for i in range(1,len(line)):
+        diff = line[i-1] - line[i]
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+def can_be_safe(line):
+    for i in range(len(line)):
+        modified_line = line[:i] + line[i+1:]
+        if safe_rm_inc(modified_line) or safe_rm_dec(modified_line):
+            return True
+    return False 
+
+def init_safe_inc(line):
+    for i in range(1,len(line)):
+        #all inc
+        if line[i] < line[i-1]:
+            return False    
+        #at most 3
+        elif line[i] - line[i-1] > 3:
+            return False 
+        #at least 1
+        elif line[i] - line[i-1] < 1:
+            return False
+    return True
+
+def init_safe_dec(line):
+    for i in range(1,len(line)):
+        #all dec 
+        if line[i] > line[i-1]:
+            return False
+        #at most 3
+        elif line[i-1] - line[i] > 3:
+            return False
+        #at least 1
+        elif line[i-1] - line[i] < 1:
+            return False
+    return True
+
+
+safe_pt2 = 0
+
 for line in lines:
     int_line = list(map(int,line.split()))
-    order = 0 #0 for dec and 1 for inc
-    init_first_value = int_line[0]
-    init_sec_value = int_line[1]
+    #PART1
+    if init_safe_inc(int_line) or init_safe_dec(int_line):
+        safe_pt2 += 1
+    #PART2
+    elif can_be_safe(int_line):
+        safe_pt2 += 1
 
-    if init_first_value < init_sec_value:
-        order = 1
-        safe += 1
-    elif init_first_value > init_sec_value:
-        order = 0
-        safe += 1
-    else:
-        order = 2
-
-
-    #dec case
-    if order == 0:
-        for i in range(1,len(int_line)):
-            #all inc 
-            if int_line[i] >= int_line[i-1]:
-                safe -= 1
-                break
-            #at most 3
-            elif int_line[i-1] - int_line[i] > 3:
-                safe -= 1
-                break
-            #at least 1
-            elif int_line[i-1] - int_line[i] < 1:
-                safe -= 1
-                break
-
-
-    #inc case
-    if order == 1:
-        for i in range(1,len(int_line)):
-            #all inc
-            if int_line[i] <= int_line[i-1]:
-                safe -= 1
-                break    
-            #at most 3
-            elif int_line[i] - int_line[i-1] > 3:
-                safe -= 1
-                break 
-            #at least 1
-            elif int_line[i] - int_line[i-1] < 1:
-                safe -= 1
-                break
-
-           
-
-print(safe)
-
-        
-
-    
+print(safe_pt2)
