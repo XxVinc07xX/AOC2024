ac1f1e8e2f29b36bc3201d5e6db87c72ae0908a3
vw <vincent.wuillaume@student.uclouvain.be>
2024-12-11 13:38:04 +0100

    d11 done

--- d11.py
+++ d11.py
@@ -1,40 +1,28 @@
+from functools import cache
+
 input = open("d11_input.txt","r")
 line = input.readline()
 line = line.split(" ")
 
-def blink_behaviour(digit):
-    inter = []
+@cache
+def blink_behaviour(digit, iter=0):
+    if iter == 75: 
+        return 1
     if digit == "0":
-        inter.append("1")
-        return inter
-    elif len(digit)%2 == 0:
-        middle = len(digit)//2
-        digit1 = digit[:middle]
-        digit2 = digit[middle:]
-        int_dig1 = int(digit1)
-        digit1 = str(int_dig1) #remove extra 0
-        int_dig2 = int(digit2)
-        digit2 = str(int_dig2)
-        inter.append(digit1)
-        inter.append(digit2)
-        return inter
-    else:
-        inter.append(str(int(digit)*2024))
-        return inter
+        return blink_behaviour("1", iter + 1) 
+    elif len(digit) % 2 == 0: 
+        middle = len(digit) // 2
+        digit1 = str(int(digit[:middle]))  # Remove leading zeros
+        digit2 = str(int(digit[middle:])) 
+        return blink_behaviour(digit1, iter + 1) + blink_behaviour(digit2, iter + 1)
+    else:  
+        new_digit = str(int(digit) * 2024)
+        return blink_behaviour(new_digit, iter + 1)
         
     
 
-for i in range(25):
-    new_line = []
-    for j in range(len(line)):
-        #print(line)
-        res = blink_behaviour(line[j])
-        if len(res) == 1:
-            new_line.append(res[0])
-        elif len(res) == 2:
-            new_line.append(res[0])
-            new_line.append(res[1])
-    #print(new_line)
-    line = new_line
+total_segments = 0
+for dig in line:
+    total_segments += blink_behaviour(dig)
 
-print(len(line))
+print(total_segments)
