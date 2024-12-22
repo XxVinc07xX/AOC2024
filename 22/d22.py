0d4aa23cb787573da31b45865cf206007d15bbd6
vw <vincent.wuillaume@student.uclouvain.be>
2024-12-22 14:42:07 +0100

    d22 done

--- 22/d22.py
+++ 22/d22.py
@@ -18,13 +18,72 @@ def secret_behavior(number):
     return step3_prune
 
 
+#PART1
+
+
 sum = 0
+overall_dic={}
+last_4 = []
+
+
 for line in lines:
+    dic={}
     val = int(line)
+    last_dig = str(val)[-1]
     for _ in range(2000):
         res = secret_behavior(val)
+        last_dig_2 = str(res)[-1]
+        diff = int(last_dig_2) - int(last_dig)
+        last_4.append(diff)
+        if len(last_4) == 4:
+            tuple_last_4 = tuple(last_4)
+            if not tuple_last_4 in dic.keys():
+                dic[tuple_last_4] = 0
+                dic[tuple_last_4] = int(last_dig_2)
+            else:
+                if int(last_dig_2) > dic[tuple_last_4]:
+                    dic[tuple_last_4] = int(last_dig_2)
+            last_4.pop(0)
+        last_dig = last_dig_2
         val = res
+    for key, value in dic.items():
+        if not key in overall_dic:
+            overall_dic[key] = value
+        else:
+            overall_dic[key] += value
     #print(val)
     sum += val
 
-print(sum)
+
+#PART2
+
+seq = max(overall_dic, key=overall_dic.get) #get max value
+#print(seq)
+sum2=0
+
+
+
+for line in lines:
+    last_4 = []
+    #print(line.strip())
+    val = int(line)
+    last_dig = str(val)[-1]
+    for _ in range(2000):
+        res = secret_behavior(val)
+        last_dig_2 = str(res)[-1]
+        diff = int(last_dig_2) - int(last_dig)
+        last_4.append(diff)
+        if len(last_4) == 4:
+            tuple_last_4 = tuple(last_4)
+            #print(tuple_last_4)
+            if tuple_last_4 == seq:
+                #print("eq seq", int(last_dig_2))
+                sum2 += int(last_dig_2)
+                break
+            last_4.pop(0)
+        last_dig = last_dig_2
+        val = res
+
+print(sum,sum2)
+
+
